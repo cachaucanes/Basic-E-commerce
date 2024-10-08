b438b04423e462765773979112147b46e8fbda71
Carlos Chaucanes - @Softimus <cachaucanes23@gmail.com>
2024-10-07 21:42:38 -0500

    add route to deploy

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -14,6 +14,7 @@ import { ProviderShoppingCard } from "../../Context";
 const AppRoutes = () => {
   let routes = useRoutes([
     { path: "/", element: <Home /> },
+    { path: "/Basic-E-commerce", element: <Home /> },
     { path: "/clothes", element: <Home /> },
     { path: "/electronics", element: <Home /> },
     { path: "/furnitures", element: <Home /> },
